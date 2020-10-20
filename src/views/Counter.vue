65a8dfeac5c2cb9656aa5dcf53547b522051d8d5
zxw <zxw@winning.com.cn>
2020-10-20 13:22:54 +0800

    Initial commit

---
+++ src/views/Counter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="counter">
+    <button @click="decrement">-</button>
+    {{ count }}
+    <button @click="increment">+</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+export default class Counter extends Vue {
+  count = 0
+
+  increment () {
+    this.count++
+  }
+
+  decrement () {
+    if (this.count === 0) return
+    this.count--
+  }
+}
+</script>
